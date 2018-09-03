554682c38d239ecc1f37182dd5268fc76f0f5e5f
Giuseppe Fagnani <giuseppef@nautes-ke.it>
2018-09-03 10:54:32 +0200

    Ora manda correttamente la data

--- modules/ws.js
+++ modules/ws.js
@@ -1,12 +1,12 @@
 var WebSocketServer = require('ws').Server;
 var wss = new WebSocketServer({port: 40510});
 
-wss.on('connection', function(ws) {
-    ws.on('message', function (message) {
+wss.on('connection', function(connection) {
+    connection.on('message', function (message) {
         console.log("received: %s", message);
     });
 
     setInterval(
-        () => ws.send('${new Date()}'), 1000
+        () => connection.send(`${new Date()}`), 1000
     )
 });
